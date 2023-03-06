fa13551461bdd95e30529236ca7849e8e082e6d2
顾宇轩 <21371106@buaa.edu.cn>
2023-03-06 19:37:32 +0800

    first

---
+++ exam.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+mkdir mydir
+chmod +w mydir
+chmod +x mydir
+chmod +r mydir
+echo 2023 > myfile
+mv moveme mydir
+cp copyme mydir
+mv mydir/copyme mydir/copied
+cat readme
+gcc bad.c 2> err.txt
+
+mkdir gen
+a=1
+if [ $# -eq 0 ] 
+then 
+	while [ $a -le 10 ]
+	do
+		touch $a.txt
+		mv $a.txt gen
+		let a=a+1
+	done
+fi
+
+if [ $# -ne 0 ]
+then 
+	while [ $a -le $1 ]
+	do
+		touch $a.txt
+		mv $a.txt gen
+		let a=a+1
+	done
+fi
+
+
