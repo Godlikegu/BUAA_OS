c20f1b97d6d8ddc2ebe2306ede034f3f2d938be7
顾宇轩 <21371106@buaa.edu.cn>
2023-03-06 20:58:37 +0800

    first

--- analyze.sh
+++ analyze.sh
@@ -2,21 +2,29 @@
 
 if [ $# -eq 1 ];
 then
-    # Your code here. (1/4)
+   grep -E "WARN|ERROR" $1 > bug.txt
 
 else
     case $2 in
     "--latest")
-        # Your code here. (2/4)
+    tail -n5 $1 
 
     ;;
     "--find")
-        # Your code here. (3/4)
+       grep "$3" $1 > $3.txt 
 
     ;;
     "--diff")
-        # Your code here. (4/4)
-
+        diff -q $1 $3;
+	if [ $? -eq 0 ] 
+		then 
+		echo same
+	fi
+	diff -q $1 $3
+	if [ $? -ne 0 ]
+		then 
+			echo different
+	fi
     ;;
     esac
 fi
