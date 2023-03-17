f7097f50fdcf3c52483a671a6b3417f9e3183ab2
顾宇轩 <21371106@buaa.edu.cn>
2023-03-17 23:52:22 +0800

    first try

--- kern/pmap.c
+++ kern/pmap.c
@@ -25,7 +25,7 @@ void mips_detect_memory() {
 
 	/* Step 2: Calculate the corresponding 'npage' value. */
 	/* Exercise 2.1: Your code here. */
-
+	npage = memsize / 0x000c;
 	printk("Memory size: %lu KiB, number of pages: %lu\n", memsize / 1024, npage);
 }
 
