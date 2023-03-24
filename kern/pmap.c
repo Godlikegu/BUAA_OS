77918d1f9ca5b636ae6b4ce83c40c30afd42d957
顾宇轩 <21371106@buaa.edu.cn>
2023-03-25 01:13:49 +0800

    add req by TA

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,17 +184,22 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 	 * page directory.
 	 * If failed to allocate a new page (out of memory), return the error. */
 	/* Exercise 2.6: Your code here. (2/3) */
-	if (!(PTE_V & *pgdir_entryp) && create){
-		if (page_alloc(&pp)==-E_NO_MEM){
+	if (!(PTE_V & *pgdir_entryp)){
+		if (create==1){
+			if (page_alloc(&pp)==-E_NO_MEM){
 			return -E_NO_MEM;
 		}
 			*pgdir_entryp = (page2pa(pp))| PTE_D | PTE_V;
 			(pp->pp_ref)++;
+		}else{
+			*ppte = NULL;
+		}
+		
 	}
 
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
-	Pte * pgtable = (Pte*) KADDR(PTE_ADDR(*pgdir_entryp)); //?
+	Pte * pgtable = (Pte*) KADDR(PTE_ADDR(*pgdir_entryp));                                                 //?
 	*ppte = pgtable + PTX(va);
 	return 0;
 }
@@ -229,7 +234,7 @@ int page_insert(Pde *pgdir, u_int asid, struct Page *pp, u_long va, u_int perm) 
 
 	/* Step 2: Flush TLB with 'tlb_invalidate'. */
 	/* Exercise 2.7: Your code here. (1/3) */
-	tlb_invalidate(*pgdir, va);                   //? why it is *pgdir
+	tlb_invalidate(*pgdir, va);                                                  //? why it is *pgdir
 
 	/* Step 3: Re-get or create the page table entry. */
 	/* If failed to create, return the error. */
